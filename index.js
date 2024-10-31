dd6dc9725bc1d3ebc7cb7a129059f1b48d73f582
1992tw <47179662+1992tw@users.noreply.github.com>
2024-10-31 16:30:31 -0500

    added cors

--- index.js
+++ index.js
@@ -86,7 +86,7 @@ async function main() {
 main();
 
 // Start the server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
