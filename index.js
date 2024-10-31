266bb979fb6c4189537605090bec3dd8f3a3fe89
1992tw <47179662+1992tw@users.noreply.github.com>
2024-10-31 17:09:13 -0500

    remove localhost ..

--- index.js
+++ index.js
@@ -94,5 +94,5 @@ main();
 // Start the server
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on port: ${PORT}`);
 });
